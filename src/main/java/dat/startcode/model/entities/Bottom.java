4ff2b44329295b140f44af128eef11c3faaf94d4
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-04-04 18:40:06 +0200

    Added the cupcake object and some bottoms + toppings

---
+++ src/main/java/dat/startcode/model/entities/Bottom.java
@@ -0,0 +1,32 @@
+package dat.startcode.model.entities;
+
+public class Bottom {
+    private int bottomPrice;
+    private String bottomName;
+
+    public Bottom(int price, String bottomName) {
+        this.bottomPrice = price;
+        this.bottomName = bottomName;
+    }
+
+    public int getBottomPrice() {
+        return bottomPrice;
+    }
+
+    public String getBottomName() {
+        return bottomName;
+    }
+
+    public void setBottomPrice(int bottomPrice) {
+        this.bottomPrice = bottomPrice;
+    }
+
+    public void setBottomName(String bottomName) {
+        this.bottomName = bottomName;
+    }
+
+    @Override
+    public String toString() {
+        return "The following bottom contains: "+bottomName+" and costs: "+ bottomPrice;
+    }
+}
