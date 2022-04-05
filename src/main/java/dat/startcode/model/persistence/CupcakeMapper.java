a446ad99c1791dff26dadd493724bab5122bb5d0
Oliverden3 <oliversjensen@hotmail.com>
2022-04-05 12:44:44 +0200

    Fixed CupcakeMapper with Jon

--- src/main/java/dat/startcode/model/persistence/CupcakeMapper.java
+++ src/main/java/dat/startcode/model/persistence/CupcakeMapper.java
@@ -3,7 +3,6 @@ package dat.startcode.model.persistence;
 import dat.startcode.model.entities.Topping;
 import dat.startcode.model.entities.User;
 import dat.startcode.model.entities.Bottom;
-import dat.startcode.model.entities.Cupcake;
 import dat.startcode.model.exceptions.DatabaseException;
 
 import java.sql.Connection;
@@ -49,50 +48,53 @@ public class CupcakeMapper {
     }
 
 
-    public ArrayList<Bottom> createBottom(int bottom_id, String bottom_name, int bottom_price) throws DatabaseException {
+    public ArrayList<Bottom> getBottom() throws DatabaseException {
+
         ArrayList<Bottom> bottomlist = new ArrayList<Bottom>();
         Logger.getLogger("web").log(Level.INFO, "");
+
         Bottom bottom;
         String sql = "SELECT * FROM bottom";
+
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                ps.setInt(1, bottom_id);
-                ps.setString(2, bottom_name);
-                ps.setInt(3, bottom_price);
-                int rowsAffected = ps.executeUpdate();
-                if (rowsAffected == 1) {
-                    bottomlist.add(bottom = new Bottom(bottom_id, bottom_name, bottom_price));
-                } else {
-                    throw new DatabaseException("The cupcake could not be inserted into the database");
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int bottomId = rs.getInt("bottom_id");
+                    String bottomName = rs.getString("bottom_name");
+                    int bottomPrice = rs.getInt("bottom_price");
+                    bottomlist.add(bottom = new Bottom(bottomId, bottomName, bottomPrice));
                 }
             }
-        } catch (SQLException ex) {
-            throw new DatabaseException(ex, "Could not insert username into database");
+        } catch (
+                SQLException ex) {
+            throw new DatabaseException(ex, "Bottoms could not be found");
         }
         return bottomlist;
     }
 
-    public ArrayList<Topping> createTopping(int topping_id, String topping_name, int topping_price) throws DatabaseException {
-        ArrayList<Topping> toppinglist = new ArrayList<Topping>();
+    public ArrayList<Topping> getToppings() throws DatabaseException {
+
+        ArrayList<Topping> toppingList = new ArrayList<Topping>();
         Logger.getLogger("web").log(Level.INFO, "");
+
         Topping topping;
         String sql = "SELECT * FROM topping";
+
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                ps.setInt(1, topping_id);
-                ps.setString(2, topping_name);
-                ps.setInt(3, topping_price);
-                int rowsAffected = ps.executeUpdate();
-                if (rowsAffected == 1) {
-                    toppinglist.add(topping = new Topping(topping_id, topping_name, topping_price));
-                } else {
-                    throw new DatabaseException("The cupcake could not be inserted into the database");
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    int toppingId = rs.getInt("topping_id");
+                    String toppingName = rs.getString("topping_name");
+                    int toppingPrice = rs.getInt("topping_price");
+                    toppingList.add(topping = new Topping(toppingId, toppingName, toppingPrice));
                 }
             }
         } catch (SQLException ex) {
-            throw new DatabaseException(ex, "Could not insert username into database");
+            throw new DatabaseException(ex, "Toppings could not be found");
         }
-        return toppinglist;
+        return toppingList;
     }
 }
 
